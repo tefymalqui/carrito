de12252ccc8e5f1d848e5bb885a837c7e9c7087b
esthepany.malqui <tefy0399@gmail.com>
2021-10-31 20:26:52 -0300

    instale MUI

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return <div>
+            <h1>Hola mundo</h1>
+        </div>;
+    }
+}
+ 
+export default NavBar;
